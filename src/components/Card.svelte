4bb2fd2e970db2f9fa95712ec3b5c6b36b0cc3a7
Mari <mari.philon@gmail.com>
2024-04-27 15:45:14 +0300

    project

---
+++ src/components/Card.svelte
@@ -0,0 +1,15 @@
+<div class='card'>
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        background-color: white;
+        color: #333;
+        border-radius: 15px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        padding: 40px 50px;
+        margin: 20px 0;
+        position: relative;
+    }
+</style>
